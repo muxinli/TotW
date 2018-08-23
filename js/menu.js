ded48ff4085df47ead885753f9811e7746c6fcaa
Muxin Li <m.muxin.li@gmail.com>
2018-08-22 22:12:06 -0500

    Revise url.js

--- js/menu.js
+++ js/menu.js
@@ -1,74 +1,74 @@
 $( document ).ready(function() {
 
-window.onload = function() {
-      document.getElementById("badges").style.display = "none";
-      document.querySelector("#createdBy").style.display = "none";
-      document.querySelector("#Nintendo").style.display = "none";
-      document.querySelector("#credits").style.display = "none";
-      document.querySelector("#thanks").style.display = "none";
+  // window.onload = function() {
+  //       document.getElementById("badges").style.display = "none";
+  //       document.querySelector("#createdBy").style.display = "none";
+  //       document.querySelector("#Nintendo").style.display = "none";
+  //       document.querySelector("#credits").style.display = "none";
+  //       document.querySelector("#thanks").style.display = "none";
 
-      if(window.innerWidth >= 1280) {  //Display badges on the right if browser size is at least 1280px
-          document.getElementById("badges").style.display = "block";
-      }
-}
+  //       if(window.innerWidth >= 1280) {  //Display badges on the right if browser size is at least 1280px
+  //           document.getElementById("badges").style.display = "block";
+  //       }
+  // }
 
-    $("#swipe").swipe({  //Opens the menu sidebar
-      swipeStatus:function(event, phase, direction, distance, duration, fingers)
-          {
-              if (phase=="move" && direction =="right") {
-                   $("#swipe").addClass("open-sidebar");
-                   $(".menu_icon").delay(150).queue(function (next) {
-                    $(this).css("visibility", "hidden");
-                    next();
-                  });
-                   return false;
-              }
-              if (phase=="move" && direction =="left") {
-                  $("#swipe").removeClass("open-sidebar");
-                  $(".menu_icon").delay(300).queue(function (next) {
-                    $(this).css("visibility", "visible");
-                    next();
-                  });
-                  return false;
-              }
-          }
+  $("#swipe").swipe({  //Opens the menu sidebar
+    swipeStatus:function(event, phase, direction, distance, duration, fingers)
+        {
+            if (phase=="move" && direction =="right") {
+                 $("#swipe").addClass("open-sidebar");
+                 $(".menu_icon").delay(150).queue(function (next) {
+                  $(this).css("visibility", "hidden");
+                  next();
+                });
+                 return false;
+            }
+            if (phase=="move" && direction =="left") {
+                $("#swipe").removeClass("open-sidebar");
+                $(".menu_icon").delay(300).queue(function (next) {
+                  $(this).css("visibility", "visible");
+                  next();
+                });
+                return false;
+            }
+        }
+  });
+
+  $(".menu_icon").on("click", function() {
+    $("#swipe").addClass("open-sidebar");
+    $(".menu_icon").delay(150).queue(function (next) {
+      $(this).css("visibility", "hidden");
+      next();
     });
+  });
 
-    $(".menu_icon").on("click", function() {
-      $("#swipe").addClass("open-sidebar");
-      $(".menu_icon").delay(150).queue(function (next) {
-        $(this).css("visibility", "hidden");
-        next();
-      });
-    });
- 
-    // $(".menu_recipes").click(function(){  //Displays recipes section when menu is clicked
-    //   $("#recipes").css("display", "block");
-    //   if(window.innerWidth >= 1280) {
-    //       document.getElementById("badges").style.display = "block";
-    //   } else {
-    //       $("#badges").css("display", "none");
-    //   }
-    //   $("#createdBy, #Nintendo, #credits, #thanks").css("display", "none");
-    // });
+  // $(".menu_recipes").click(function(){  //Displays recipes section when menu is clicked
+  //   $("#recipes").css("display", "block");
+  //   if(window.innerWidth >= 1280) {
+  //       document.getElementById("badges").style.display = "block";
+  //   } else {
+  //       $("#badges").css("display", "none");
+  //   }
+  //   $("#createdBy, #Nintendo, #credits, #thanks").css("display", "none");
+  // });
 
-    // $(".menu_badges").click(function(){  //Displays badges section when badges menu is clicked
-    //   $("#recipes").css("display", "none");
-    //   $("#badges").css("display", "block");
-    //   $("#createdBy, #Nintendo, #credits, #thanks").css("display", "none");
-    // });
+  // $(".menu_badges").click(function(){  //Displays badges section when badges menu is clicked
+  //   $("#recipes").css("display", "none");
+  //   $("#badges").css("display", "block");
+  //   $("#createdBy, #Nintendo, #credits, #thanks").css("display", "none");
+  // });
 
-    // $(".menu_about").click(function(){  //Displays about section when menu option is clicked
-    //   $("#recipes").css("display", "none");
-    //   $("#badges").css("display", "none");
-    //   $("#createdBy, #Nintendo, #credits, #thanks").css("display", "block");
-    // });
+  // $(".menu_about").click(function(){  //Displays about section when menu option is clicked
+  //   $("#recipes").css("display", "none");
+  //   $("#badges").css("display", "none");
+  //   $("#createdBy, #Nintendo, #credits, #thanks").css("display", "block");
+  // });
 
-    document.body.onresize = function(){  //Hides or displays badges when browser is resized
-      if((window.innerWidth < 1280 && $("#recipes").css("display") == "block") || $("#createdBy, #Nintendo, #credits, #thanks").css("display") == "block") {  //Display badges on the right if browser size is at least 1280px
-          document.getElementById("badges").style.display = "none";
-      } else {
-          document.getElementById("badges").style.display = "block";
-      }
+  document.body.onresize = function(){  //Hides or displays badges when browser is resized
+    if((window.innerWidth < 1280 && $("#recipes").css("display") == "block") || $("#createdBy, #Nintendo, #credits, #thanks").css("display") == "block") {  //Display badges on the right if browser size is at least 1280px
+        document.getElementById("badges").style.display = "none";
+    } else {
+        document.getElementById("badges").style.display = "block";
     }
+  }
 });
--- js/url.js
+++ js/url.js
@@ -1,64 +1,63 @@
 $( document ).ready(function() {
-	$(window).trigger('hashchange');
-
 	window.onload = function() {
-      location.hash = "#";  //Sets url to home page "/#"
-	}
+        $(window).trigger('hashchange');
+  	}
 
-
-	// function updateUrl (url) {  //Updates URL without reloading
-	//       if (typeof (history.pushState) != "undefined") {
-	//         var obj = { Url: url };
-	//         history.pushState(obj, obj.Page, obj.Url);
-	//       } else {  //Alerts if browser is not HTML5 compatible
-	//         alert("This browser does not support HTML5. Please use the latest version of Chrome.");
-	//       }
-	//     }
+	function updateUrl (page, url) {  //Updates URL without reloading
+	      if (typeof (history.pushState) != "undefined") {
+	        var obj = { Page: page, Url: url };
+	        history.pushState(obj, obj.Page, obj.Url);
+	      } else {  //Alerts if browser is not HTML5 compatible
+	        alert("This browser does not support HTML5. Please use the latest version of Chrome.");
+	      }
+	    }
 
     $(function () {  //Changes URL when menu is clicked
         $(".menu_recipes").click(function () {
-        	location.hash = "recipes";
-            // updateUrl('#recipes');
+            updateUrl('Page1', 'recipes');
         });
         $(".menu_badges").click(function () {
-        	location.hash = "badges";
-            // updateUrl('#badges');
+            updateUrl('Page2', 'badges');
         });
         $(".menu_about").click(function () {
-         	location.hash = "about";
-           // updateUrl('#about');
+            updateUrl('Page3', 'about');
         });
     });
 
     $(window).on('hashchange', function() {  //Runs render() whenever the URL changes (detects with hashchange). 
-    	render(decodeURI(window.location.hash));  //Runs render() on the current URL. Use decodeURI to get a valid hash without special characters; needed for browser compatibility.
+    	render(decodeURI(window.location.search));  //Runs render() on the current URL. Use decodeURI to get a valid hash without special characters; needed for browser compatibility.
     });
 
     function render(url) {
-	    if (location.hash == "#") {
-			document.getElementById("badges").style.display = "none";
-			document.querySelector("#createdBy").style.display = "none";
-			document.querySelector("#Nintendo").style.display = "none";
-			document.querySelector("#credits").style.display = "none";
-			document.querySelector("#thanks").style.display = "none";
+	    if (window.location.search == "#") {
+	    	document.getElementById("badges").style.display = "none";
+	        document.querySelector("#createdBy").style.display = "none";
+	        document.querySelector("#Nintendo").style.display = "none";
+	        document.querySelector("#credits").style.display = "none";
+	        document.querySelector("#thanks").style.display = "none";
 
-			if(window.innerWidth >= 1280) {  //Display badges on the right if browser size is at least 1280px
-			  document.getElementById("badges").style.display = "block";
-			}
-	    } else if (location.hash == "#recipes") {  //Displays recipes section at recipes url
-	    	$("#recipes").css("display", "block");
-	    	$("#createdBy, #Nintendo, #credits, #thanks").css("display", "none");
+	        if(window.innerWidth >= 1280) {  //Display badges on the right if browser size is at least 1280px
+	            document.getElementById("badges").style.display = "block";
+	        }
+	    }
 
-	    	if(window.innerWidth >= 1280) {
+	    else if (window.location.search == "recipes#") {  //Displays recipes section at recipes url
+	      $("#recipes").css("display", "block");
+	      if(window.innerWidth >= 1280) {
 	          document.getElementById("badges").style.display = "block";
-	      	} else {
-	            $("#badges").css("display", "none");
-	      	}
-	    } else if (location.hash == "#badges") {  //Displays badges section at badges url
+	      } else {
+	          $("#badges").css("display", "none");
+	      }
+	      $("#createdBy, #Nintendo, #credits, #thanks").css("display", "none");
+	    }
+
+	    else if (window.location.search == "badges#") {  //Displays badges section at badges url
 	      $("#recipes").css("display", "none");
 	      $("#badges").css("display", "block");
 	      $("#createdBy, #Nintendo, #credits, #thanks").css("display", "none");
-	    } else if (location.hash == "#about") {  //Displays about section at about url
+	    }
+
+	    else if (window.location.search == "about#") {  //Displays about section at about url
 	      $("#badges").css("display", "none");
 	      $("#createdBy, #Nintendo, #credits, #thanks").css("display", "block");
 	    }
