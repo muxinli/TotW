22be134c17743c946c6f78a25620daec8e9b6bd0
Muxin Li <m.muxin.li@gmail.com>
2018-08-24 00:08:29 -0500

    Add location based SPA
    URL updates without reload or hash tag at end
    
    Can use browser navigation buttons to go back and forth in content history

--- js/url.js
+++ js/url.js
@@ -1,29 +1,30 @@
 $( document ).ready(function() {
- 	var url = window.location.pathname;  //Gets the final part of the url... but it's not changing with changes to the url
- 	console.log('url last is' + url);
+	var menu = document.querySelectorAll(".menu_recipes, .menu_badges, .menu_about");
 
-	$(".menu_recipes").click(function () {  //Updates URL without reload upon menu item click... but appears with a hashtag
-		history.pushState(null, null, '/recipes');
-   		console.log("location is " + url);
+	for (var i = 0; i < menu.length; i++) {  //Add click event listener to each of the menu buttons
+		menu[i].addEventListener('click', clickHandler, true);  
+	}
+
+	function clickHandler(event) {  //Updates URL without reload upon menu item click
+	   	var url = event.target.getAttribute('href').split('/').pop() || null;
    		load(url);  //Trigger function to display content based on url
-	});
-	$(".menu_badges").click(function () {
-		history.pushState(null, null, '/badges');
-		console.log("location is " + url);
-   		load(url);
-	});
-	$(".menu_about").click(function () {
-		history.pushState(null, null, '/about');
-		console.log("location is " + url);
-   		load(url);
-	});
 
-	window.onload = load(url);  //Triggers load(url) on first load
+		history.pushState(url, event.target.textContent, event.target.href);  //Updates URL address and stores it into browser history
+
+   		return event.preventDefault();  //Prevents default actions, like adding a '#' at the end of URL
+	};
+
+	window.onload = load("");  //Loads home page
 	
+	window.addEventListener('popstate', function(event) {  //Allows for users to use browser navigation buttons to go back and forth between the content they've visited
+      console.log('popstate fired!');  //Just a test that the popstate event listener is working
+      load(event.state);  //The event.state here is the pushState command's 'url' in the clickHandler function
+    });
     
     function load(url) {  //Determine which content displays based on URL
-	    if (url == "/") {  //If using location.pathname, use '/' for homepage
-	    	document.getElementById("badges").style.display = "none";
+		if (url == "" || url == null) {  //Displays home page (recipes)
+			document.getElementById("recipes").style.display = "block";
+			document.getElementById("badges").style.display = "none";
 	        document.querySelector("#createdBy").style.display = "none";
 	        document.querySelector("#Nintendo").style.display = "none";
 	        document.querySelector("#credits").style.display = "none";
@@ -31,13 +32,13 @@ $( document ).ready(function() {
 
 	        if(window.innerWidth >= 1280) {  //Display badges on the right if browser size is at least 1280px
 	            document.getElementById("badges").style.display = "block";
-	        }
+	        }		
 	    }
 
 	    else if (url == "recipes") {  //Displays recipes section at recipes url
 	      $("#recipes").css("display", "block");
 	      if(window.innerWidth >= 1280) {
-	          document.getElementById("badges").style.display = "block";
+	          document.getElementById("badges").style.display = "block";  //Displays badges if screen size is large
 	      } else {
 	          $("#badges").css("display", "none");
 	      }
@@ -51,7 +52,7 @@ $( document ).ready(function() {
 	    }
 
 	    else if (url == "about") {  //Displays about section at about url
-	      $("#badges").css("display", "none");
+	      $("#badges, #recipes").css("display", "none");
 	      $("#createdBy, #Nintendo, #credits, #thanks").css("display", "block");
 	    }
     }
