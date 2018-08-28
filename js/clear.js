f042d754e7ee2ce9f953701b1e8c3a4795cf2167
Muxin Li <m.muxin.li@gmail.com>
2018-08-28 13:53:22 -0500

    Redirect clear refresh
    Redirect to home URL
    Moved clear progress button to top of page

--- js/clear.js
+++ js/clear.js
@@ -2,10 +2,10 @@ $( document ).ready(function() {
 
 document.getElementById("btn").addEventListener("click", function() {  //Confirmation checkbox when clear button is clicked on. Will clear localStorage and refresh if confirmed.
     var txt;
-    if (confirm("Hit OK to reset progress (page will reload).")) {
+    if (confirm("Hit OK to reset progress (will redirect to home page).")) {
         localStorage.clear();
         txt = "Resetting progress. What's for dinner?";
-        setTimeout(function() { location.reload(); }, 500);
+        setTimeout(function() { window.location.href = "https://shrouded-earth-77506.herokuapp.com"; }, 500);
     } else {
         txt = "Reset canceled. Happy cooking!";
     }
